1ae330c910390ea67b4d0657423647e16bd924dd
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2021-01-26 16:58:19 +0000

    Implement UC3

--- gateways/external_api.rb
+++ gateways/external_api.rb
@@ -5,7 +5,8 @@ require 'rest-client'
 class ExternalApiGateway
   CREATE_ENDPOINT = 'https://run.mocky.io/v3/727fd888-f609-4896-82d5-8d051e38036f'
   UPDATE_ENDPOINT = 'https://run.mocky.io/v3/7e28a289-c560-40d3-95b9-e7544d4357c3'
-
+  DELETE_ENDPOINT = 'https://run.mocky.io/v3/2e4472a4-ddc5-4f87-a7d5-baa60a7f7847'
+  
   def create(data)
     RestClient.post(
       CREATE_ENDPOINT,
@@ -20,5 +21,9 @@ class ExternalApiGateway
       data,
       content_type: :json, accept: :json
     )
+  end
+
+  def delete()
+    RestClient.delete DELETE_ENDPOINT
   end
 end
