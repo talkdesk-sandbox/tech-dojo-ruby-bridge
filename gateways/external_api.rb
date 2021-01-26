acd20a74f6b955032ef28a46e236f2a8054dea7f
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2021-01-26 16:59:06 +0000

    Lint previous changes

--- gateways/external_api.rb
+++ gateways/external_api.rb
@@ -6,7 +6,7 @@ class ExternalApiGateway
   CREATE_ENDPOINT = 'https://run.mocky.io/v3/727fd888-f609-4896-82d5-8d051e38036f'
   UPDATE_ENDPOINT = 'https://run.mocky.io/v3/7e28a289-c560-40d3-95b9-e7544d4357c3'
   DELETE_ENDPOINT = 'https://run.mocky.io/v3/2e4472a4-ddc5-4f87-a7d5-baa60a7f7847'
-  
+
   def create(data)
     RestClient.post(
       CREATE_ENDPOINT,
@@ -23,7 +23,7 @@ class ExternalApiGateway
     )
   end
 
-  def delete()
+  def delete
     RestClient.delete DELETE_ENDPOINT
   end
 end
