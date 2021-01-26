0c8f8e27273d565ebbcafff687d87f486f0b57d0
NelsonBarreira <43042412+NelsonBarreira@users.noreply.github.com>
2021-01-26 16:36:00 +0000

    Add Update ticket (#10)

--- gateways/external_api.rb
+++ gateways/external_api.rb
@@ -4,10 +4,19 @@ require 'rest-client'
 
 class ExternalApiGateway
   CREATE_ENDPOINT = 'https://run.mocky.io/v3/727fd888-f609-4896-82d5-8d051e38036f'
+  UPDATE_ENDPOINT = 'https://run.mocky.io/v3/7e28a289-c560-40d3-95b9-e7544d4357c3'
 
   def create(data)
     RestClient.post(
       CREATE_ENDPOINT,
+      data,
+      content_type: :json, accept: :json
+    )
+  end
+
+  def update(data)
+    RestClient.put(
+      UPDATE_ENDPOINT,
       data,
       content_type: :json, accept: :json
     )
